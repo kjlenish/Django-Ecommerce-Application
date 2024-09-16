4f0327d6c9c38cba99e2a40c39cedc05c94bd683
Lenish K J <lenish.kj@flytxt.com>
2024-09-16 13:28:52 +0530

    modified templates for production

--- ecommerce_application/settings.py
+++ ecommerce_application/settings.py
@@ -65,7 +65,7 @@ ROOT_URLCONF = 'ecommerce_application.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
